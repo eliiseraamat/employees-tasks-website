f15910582b81e82bc4d6bdd9082d3adfcf9eb9f5
elraam <elraam@taltech.ee>
2025-02-09 18:06:11 +0200

    Restore ex6 directory from previous commit

---
+++ ex6/connection.php
@@ -0,0 +1,13 @@
+<?php
+
+const USERNAME = 'elraam';
+const PASSWORD = 'd1eccf';
+
+function getConnection(): PDO {
+    $host = 'db.mkalmo.eu';
+
+    $address = sprintf('mysql:host=%s;port=3306;dbname=%s',
+        $host, USERNAME);
+
+    return new PDO($address, USERNAME, PASSWORD);
+}
